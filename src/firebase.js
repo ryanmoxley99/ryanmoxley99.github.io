da48a8a1ebb0c622a49cf638f56f8c2d85e09ce3
Ryan Moxley <ryanmoxley99@gmail.com>
2023-05-08 22:20:56 -0400

    linked to my firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,17 @@ import { getAuth } from "firebase/auth";
 import {ref, getDatabase} from 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCzqE60--_MYmqFisNQQw7sz11NyoFXMPA",
-  authDomain: "cmmc-clue-less.firebaseapp.com",
-  databaseURL: "https://cmmc-clue-less-default-rtdb.firebaseio.com",
-  projectId: "cmmc-clue-less",
-  storageBucket: "cmmc-clue-less.appspot.com",
-  messagingSenderId: "444659808409",
-  appId: "1:444659808409:web:7cc53b62815173665f2008"
+  apiKey: "AIzaSyBmWP-MYQxiW1JEkFX64RfyM7ekyAbSfZk",
+  authDomain: "clueless-hopkins.firebaseapp.com",
+  databaseURL: "https://clueless-hopkins-default-rtdb.firebaseio.com",
+  projectId: "clueless-hopkins",
+  storageBucket: "clueless-hopkins.appspot.com",
+  messagingSenderId: "945920393435",
+  appId: "1:945920393435:web:35b99d58897b3caedf8cfe",
+  measurementId: "G-L9XTC1SMZW"
 };
+
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db = getDatabase(app)
